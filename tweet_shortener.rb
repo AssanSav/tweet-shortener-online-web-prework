1abb102951fd81d3212d98fe6788bf30a7e061ea
assane savadogo <assane.savadogo81@gmail.com>
2019-06-06 20:43:01 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+# Write y"hello" becomes 'hi'
+"to, two, too" become '2' 
+"for, four" become '4'
+'be' becomes 'b'
+'you' becomes 'u'
+"at" becomes "@" 
+"and" becomes "&"our code here.
