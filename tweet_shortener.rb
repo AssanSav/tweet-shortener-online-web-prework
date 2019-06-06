dba721c00a5ed758284005e03d581d52400e4e0b
assane savadogo <assane.savadogo81@gmail.com>
2019-06-06 20:47:48 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,11 @@
 def dictionary
-  dictionary = {"hello" =>'hi',
-"to, two, too" => '2', 
-"for, four" => '4',
+  dictionary = {
+"hello" =>'hi',
+"to" => '2',
+"two" => '2',
+"too" => '2'
+"for" => '4',
+"four" => '4'
 'be' => 'b',
 'you' => 'u',
 "at" => "@", 
