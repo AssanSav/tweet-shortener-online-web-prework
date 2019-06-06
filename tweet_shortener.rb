8d5e46ebf2063c84c8aeb37d19695a32d11205a2
assane savadogo <assane.savadogo81@gmail.com>
2019-06-06 20:45:14 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,10 +17,3 @@ def word_substituter(string)
     end 
   end.join(" ")
 end 
-word_substituter(tweet_one)
-
-def bulk_tweet_shortener(array)
-  array.each do |tweet|
-    puts word_substituter(tweet)
-  end
-end
