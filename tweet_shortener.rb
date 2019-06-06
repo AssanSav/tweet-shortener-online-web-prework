93225d04f499ee9c16526fcc676659de0f0f98aa
assane savadogo <assane.savadogo81@gmail.com>
2019-06-06 20:45:11 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -8,3 +8,19 @@ def dictionary
 "and" =>"&"}
 end
 
+def word_substituter(string) 
+  string.split.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else 
+      word 
+    end 
+  end.join(" ")
+end 
+word_substituter(tweet_one)
+
+def bulk_tweet_shortener(array)
+  array.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
